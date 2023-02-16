cee9d49f51d1b11225185f65f87e7c6e7fe26a9b
DmityFilchev <filchev_dmitry@mail.ru>
2023-02-16 15:56:54 +0300

    Добавьте файлы проекта.

---
+++ Task29_2/Task29_2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+class Shape
+{
+protected:
+	struct BoundingBoxDimension
+	{
+		double width = 0;
+		double length = 0;
+	};
+		
+public:
+	
+	
+	virtual double square() = 0;
+	virtual BoundingBoxDimension dimensions() = 0;
+
+};
+class Circle : public Shape
+{
+
+};
+class Rectangle :public Shape
+{
+
+};
+class Triangle :public Shape
+{
+
+};
+
+int main()
+{
+	
+	return 0;
+}
